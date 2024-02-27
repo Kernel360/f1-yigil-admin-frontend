ffdddf32f28004c4bfbf2495a4dee8758055ceeb
minson96 <vlrmakdhkd@gmail.com>
2024-02-27 16:26:48 +0900

    last

--- yigil-admin/src/components/snippet/UserInfo.tsx
+++ yigil-admin/src/components/snippet/UserInfo.tsx
@@ -71,7 +71,7 @@ const UserInfo: React.FC = () => {
           </PopoverContent>
         </Popover>
       ) : (
-        <Link to="/admin/login">
+        <Link to="/login">
           <Button>
             <EnvelopeOpenIcon className="mr-2 h-4 w-4" /> Login with Email
           </Button>
